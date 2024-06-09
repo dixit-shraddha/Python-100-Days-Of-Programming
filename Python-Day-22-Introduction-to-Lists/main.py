be404123f5810f519cb35a3ea8b8acdd472cc809
Shraddha Dixit <167637387+dixit-shraddha@users.noreply.github.com>
2024-06-09 16:52:29 +0530

    Update main.py

--- Python-Day-22-Introduction-to-Lists/main.py
+++ Python-Day-22-Introduction-to-Lists/main.py
@@ -1,3 +1,56 @@
+li_1 = [1,2,3,4,5]
+li_2 = ['a','b','c']
+li_3 = [1,2,'a',True]
+
+print(li_1)
+print(li_2)
+print(li_3)
+
+for i in li_1 :
+  print(i)
+
+print("\n")
+print(li_1[2])
+print(li_1[-2])
+print(li_1[1:3])
+print(li_1[:])
+print(li_1[:-1])
+
+if 2 in li_1 :
+  print("yes")
+else:
+  print("No")
+
+if 'y' in li_2:
+  print("yes")
+else:
+  print("no")
+
+
+li_4 = [1,2,3,4,5,6,7,8,9,10]
+print(li_4)
+print(li_4[0:10:2])
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[4:])	#using positive indexes
+print(animals[-4:])	#using negative indexes
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[:6])	#using positive indexes
+print(animals[:-3])	#using negative indexes
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[1:8:3])
+
+names = ["Milo", "Sarah", "Bruno", "Anastasia", "Rosa"]
+namesWith_O = [item for item in names if "o" in item]
+print(namesWith_O)
+nameswith_a = [item for item in names if "a" in item]
+print(nameswith_a)
+namesWith_1 = [item for item in names if (len(item) > 4)]
+print(namesWith_1)
+
+
 marks = [3, 5, 6, "Harry", True, 6, 7 , 2, 32, 345, 23]
 # print(marks)
 # print(type(marks))
