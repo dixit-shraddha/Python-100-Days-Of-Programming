7a375cc4ba84bf0a35fbe454ecb58eb19bab5550
Shraddha Dixit <167637387+dixit-shraddha@users.noreply.github.com>
2024-06-09 17:26:35 +0530

    Update main.py

--- Python-Day-23-List-Methods/main.py
+++ Python-Day-23-List-Methods/main.py
@@ -1,3 +1,45 @@
+colors = ["voilet", "indigo", "blue", "green"]
+
+print(colors)
+colors.sort()
+print(colors)
+colors.sort(reverse=True)
+print(colors)
+
+num = [4, 2, 5, 3, 6, 1, 2, 1, 2, 8, 9, 7]
+num.sort()
+print(num)
+num.sort(reverse=True)
+print(num)
+
+colors = ["voilet", "indigo", "blue", "green"]
+colors.reverse()
+print(colors)
+
+num = [4, 2, 5, 3, 6, 1, 2, 1, 2, 8, 9, 7]
+num.reverse()
+print(num)
+
+colors = ["voilet", "green", "indigo", "blue", "green"]
+print(colors.index("indigo"))
+
+num = [4, 2, 5, 3, 6, 1, 2, 1, 3, 2, 8, 9, 7]
+print(num.index(3))
+
+colors = ["voilet", "green", "indigo", "blue", "green"]
+print(colors.count("green"))
+
+newlist = colors.copy()
+print(newlist)
+newlist.append("red")
+print(newlist)
+newlist.insert(1, "orange")
+print(newlist)
+colors.extend(num)
+print(colors)
+print(colors + num)
+
+
 l = [11, 45, 1, 2, 4, 6, 1, 1]
 print(l)
 # l.append(7)
