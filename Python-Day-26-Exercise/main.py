5aec0d4c01b824db4fbd67c4b3cc7cbd8539bae4
Shraddha Dixit <167637387+dixit-shraddha@users.noreply.github.com>
2024-06-17 19:42:42 +0530

    Update main.py

--- Python-Day-26-Exercise/main.py
+++ Python-Day-26-Exercise/main.py
@@ -10,3 +10,18 @@ elif(hour>=12 and hour<17):
   print("Good Afternoon Sir!")
 elif(hour>=17 and hour<0):
   print("Good Night Sir!")
+
+import time
+hour = int(time.strftime('%H')) + 6
+print(hour)
+# t = time.strftime('%H:%M:%S')
+# print(t)
+
+if (hour >=0 and hour <12):
+  print("Good Morning")
+elif (hour >=12 and hour <15):
+  print("Good Afternoon")
+elif (hour >= 15 and hour <17):
+  print("Good Evening")
+else:
+  print("Good Night")
