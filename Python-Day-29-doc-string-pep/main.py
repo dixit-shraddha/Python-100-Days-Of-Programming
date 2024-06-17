88f43987098c49a01301b593fb6d756722d1c142
Shraddha Dixit <167637387+dixit-shraddha@users.noreply.github.com>
2024-06-17 19:48:10 +0530

    Update main.py

--- Python-Day-29-doc-string-pep/main.py
+++ Python-Day-29-doc-string-pep/main.py
@@ -4,3 +4,11 @@ def square(x):
 
 print(square.__doc__)
 print(square(5))
+
+def square(x):
+  '''Hello you are seeing the docstring '''
+  return x*x
+
+print(square(5))
+print(square.__doc__)
+print(square(5))
