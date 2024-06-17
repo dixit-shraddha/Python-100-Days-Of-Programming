c4b9807ee004edb08695ea53c75df2f72c365fc5
Shraddha Dixit <167637387+dixit-shraddha@users.noreply.github.com>
2024-06-17 19:46:03 +0530

    Update main.py

--- Python-Day-28-f-strings/main.py
+++ Python-Day-28-f-strings/main.py
@@ -10,3 +10,22 @@ txt = f"For only {price:.2f} dollars!"
 print(txt)
 # print(txt.format())
 print(type(f"{2 * 30}"))
+
+
+school = "KIT college of engineering"
+name = "Shraddha"
+sentence = "My name is {0} and I am studying in {1} \n"
+print(sentence.format(name, school))
+# print(sentence.format(0,1))
+name = "Smith"
+school = "ABC"
+
+sentence1 = f"My name is {{name}} and I am studying in {{school}} \n"
+
+sentence = f"My name is {name} and I am studying in {school} \n"
+
+print(sentence1)
+print(sentence)
+print(type(sentence))
+print(f"{2*60}")
+print(type(f"{2*60}"))
